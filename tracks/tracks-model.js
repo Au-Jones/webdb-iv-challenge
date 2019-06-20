c00e249f5e94542544b2b730a25140fd4084c68b
Your Name <aujones95@gmail.com>
2019-06-20 19:30:17 -0400

    got half done

--- tracks/tracks-model.js
+++ tracks/tracks-model.js
@@ -8,3 +8,22 @@ module.exports = {
     addRecipe
 }
 
+function getDishes() {
+    return db('tracks')
+}
+
+function getDish(ID) {
+    return db('tracks').where({ id }).first()
+}
+
+function addDish(dish) {
+    return db('tracks').insert(track)
+}
+
+function getRecipes() {
+    return db('tracks').where({ id }).filter()
+}
+
+function addRecipe(reicpe) {
+    return db('tracks').instert(track)
+}
